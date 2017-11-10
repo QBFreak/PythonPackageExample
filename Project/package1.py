cafd47ff38ee82442adfd0fe924ccaea6d03fb60
Jason Hill <qbfreak@qbfreak.net>
2017-11-10 16:44:18 -0500

    Initial commit

---
+++ Project/package1.py
@@ -0,0 +1,3 @@
+class myClass:
+    def __init__(self):
+        print("This is an instance of Project.package1.myClass")
---
+++ test.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+import Project.package1
+
+myInst = Project.package1.myClass()
